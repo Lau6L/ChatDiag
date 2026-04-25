f8dbec0d9eea9eea1edff6a60a5e552a6e51a4dc
6-LL <lorenzanilautaro+github@gmail.com>
2026-04-25 14:41:00 -0300

    mod API added, Dialogs#dialogs now private

--- src/main/java/io/github/lau6l/chatdiag/ChatDiag.java
+++ src/main/java/io/github/lau6l/chatdiag/ChatDiag.java
@@ -1,15 +1,23 @@
 package io.github.lau6l.chatdiag;
 
+import io.github.lau6l.chatdiag.api.ChatDiagApi;
 import io.github.lau6l.chatdiag.command.ChatDiagCommand;
+import io.github.lau6l.chatdiag.dialog.Dialog;
+import io.github.lau6l.chatdiag.dialog.DialogExecutor;
 import io.github.lau6l.chatdiag.dialog.DialogLoader;
+import io.github.lau6l.chatdiag.dialog.Dialogs;
 import io.github.lau6l.chatdiag.util.Scheduler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ChatDiag implements ModInitializer {
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+public class ChatDiag implements ModInitializer, ChatDiagApi {
     public static final String MOD_ID = "chatdiag";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -30,5 +38,45 @@ public class ChatDiag implements ModInitializer {
 
     public static Identifier of(String value) {
         return Identifier.of(MOD_ID, value);
+    }
+
+    private static final ChatDiagApi INSTANCE = new ChatDiag();
+    public static ChatDiagApi api() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Dialog getDialog(Identifier id) {
+        return Dialogs.getDialog(id);
+    }
+
+    @Override
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Identifier id, Collection<ServerPlayerEntity> players) {
+        return DialogExecutor.startDialog(id, players);
+    }
+
+    @Override
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Dialog dialog, Collection<ServerPlayerEntity> players) {
+        return DialogExecutor.startDialog(dialog, players);
+    }
+
+    @Override
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Identifier id, Collection<ServerPlayerEntity> players, CompletableFuture<Collection<ServerPlayerEntity>> future) {
+        return DialogExecutor.startDialogWithFuture(id, players, future);
+    }
+
+    @Override
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Dialog dialog, Collection<ServerPlayerEntity> players, CompletableFuture<Collection<ServerPlayerEntity>> future) {
+        return DialogExecutor.startDialogWithFuture(dialog, players, future);
+    }
+
+    @Override
+    public String serializeDialog(Dialog dialog) {
+        return DialogLoader.serializeDialog(dialog);
+    }
+
+    @Override
+    public Dialog deserializeDialog(String json) {
+        return DialogLoader.deserializeDialog(json);
     }
 }
