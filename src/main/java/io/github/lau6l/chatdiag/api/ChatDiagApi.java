f00976b47234673e7cf1121445bb87c18d0557bb
6-LL <lorenzanilautaro+github@gmail.com>
2026-04-27 16:13:21 -0300

    api logic moved to ChatDiagApi class

--- src/main/java/io/github/lau6l/chatdiag/api/ChatDiagApi.java
+++ src/main/java/io/github/lau6l/chatdiag/api/ChatDiagApi.java
@@ -1,18 +1,50 @@
 package io.github.lau6l.chatdiag.api;
 
 import io.github.lau6l.chatdiag.dialog.Dialog;
+import io.github.lau6l.chatdiag.dialog.DialogExecutor;
+import io.github.lau6l.chatdiag.dialog.DialogLoader;
+import io.github.lau6l.chatdiag.dialog.Dialogs;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
-public interface ChatDiagApi {
-    Dialog getDialog(Identifier id);
-    CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Identifier id, Collection<ServerPlayerEntity> players);
-    CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Dialog dialog, Collection<ServerPlayerEntity> players);
-    CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Identifier id, Collection<ServerPlayerEntity> players, CompletableFuture<Collection<ServerPlayerEntity>> future);
-    CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Dialog dialog, Collection<ServerPlayerEntity> players, CompletableFuture<Collection<ServerPlayerEntity>> future);
-    String serializeDialog(Dialog dialog);
-    Dialog deserializeDialog(String json);
+public class ChatDiagApi {
+    private static final ChatDiagApi INSTANCE = new ChatDiagApi();
+
+    private ChatDiagApi() {
+    }
+
+    public static ChatDiagApi api() {
+        return INSTANCE;
+    }
+
+    public Dialog getDialog(Identifier id) {
+        return Dialogs.getDialog(id);
+    }
+
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Identifier id, Collection<ServerPlayerEntity> players) {
+        return DialogExecutor.startDialog(id, players);
+    }
+
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Dialog dialog, Collection<ServerPlayerEntity> players) {
+        return DialogExecutor.startDialog(dialog, players);
+    }
+
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Identifier id, Collection<ServerPlayerEntity> players, CompletableFuture<Collection<ServerPlayerEntity>> future) {
+        return DialogExecutor.startDialogWithFuture(id, players, future);
+    }
+
+    public CompletableFuture<Collection<ServerPlayerEntity>> startDialog(Dialog dialog, Collection<ServerPlayerEntity> players, CompletableFuture<Collection<ServerPlayerEntity>> future) {
+        return DialogExecutor.startDialogWithFuture(dialog, players, future);
+    }
+
+    public String serializeDialog(Dialog dialog) {
+        return DialogLoader.serializeDialog(dialog);
+    }
+
+    public Dialog deserializeDialog(String json) {
+        return DialogLoader.deserializeDialog(json);
+    }
 }
