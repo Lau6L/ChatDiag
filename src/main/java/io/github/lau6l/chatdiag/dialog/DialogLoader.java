5e3e2ac235fcfd10327b4ef962adaf8381c9e07f
6-LL <lorenzanilautaro+github@gmail.com>
2026-04-26 21:29:10 -0300

    modrinth publishing added, DialogLoader#serializeDialog string return fixed

--- src/main/java/io/github/lau6l/chatdiag/dialog/DialogLoader.java
+++ src/main/java/io/github/lau6l/chatdiag/dialog/DialogLoader.java
@@ -108,7 +108,7 @@ public class DialogLoader {
                 .encodeStart(JsonOps.INSTANCE, dialog)
                 .result()
                 .orElseThrow()
-                .getAsString();
+                .toString();
     }
 
     /**
