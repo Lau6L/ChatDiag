fbed8f7f47cc9aa4b752c6fb5ce004911cc10a83
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-12 19:00:37 -0300

    Dialogs methods renamed
    (cherry picked from commit 306f9511712c36c115fcf5611c22090244a67aab)

--- src/main/java/io/github/lau6l/chatdiag/dialog/Dialogs.java
+++ src/main/java/io/github/lau6l/chatdiag/dialog/Dialogs.java
@@ -1,5 +1,6 @@
 package io.github.lau6l.chatdiag.dialog;
 
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 
 import java.util.HashMap;
@@ -12,11 +13,14 @@ import java.util.Set;
 public class Dialogs {
     private static Map<Identifier, Dialog> dialogs;
 
-    public static Set<Identifier> getDialogIds() {
+    public static Set<Identifier> getIds() {
         return dialogs.keySet();
     }
-    public static Dialog getDialog(Identifier id) {
+    public static Dialog get(Identifier id) {
         return dialogs.get(id);
+    }
+    public static Dialog get(Identifier id, ServerCommandSource source) {
+        return dialogs.get(id).withSource(source);
     }
 
     public static void setDialogs(Map<Identifier, Dialog> newDialogs) {
