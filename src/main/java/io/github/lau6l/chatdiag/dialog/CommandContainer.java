1603c49039a811dccbdc978e5fc741aff9411e84
6-LL <lorenzanilautaro+github@gmail.com>
2026-05-12 18:23:32 -0300

    CommandContainer.dispatcher removed

--- src/main/java/io/github/lau6l/chatdiag/dialog/CommandContainer.java
+++ src/main/java/io/github/lau6l/chatdiag/dialog/CommandContainer.java
@@ -1,23 +1,19 @@
 package io.github.lau6l.chatdiag.dialog;
 
-import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.ServerCommandSource;
 
 public class CommandContainer {
     public final String command;
     private ServerCommandSource source;
-    private CommandDispatcher<ServerCommandSource> dispatcher;
 
-    public CommandContainer(String command, ServerCommandSource source, CommandDispatcher<ServerCommandSource> dispatcher) {
+    public CommandContainer(String command, ServerCommandSource source) {
         this.command = command;
         this.source = source;
-        this.dispatcher = dispatcher;
     }
 
     public CommandContainer(String command) {
         this.command = command;
         this.source = null;
-        dispatcher = null;
     }
 
     public ServerCommandSource source() {
@@ -26,13 +22,5 @@ public class CommandContainer {
 
     public void source(ServerCommandSource source) {
         this.source = source;
-    }
-
-    public CommandDispatcher<ServerCommandSource> dispatcher() {
-        return dispatcher;
-    }
-
-    public void dispatcher(CommandDispatcher<ServerCommandSource> dispatcher) {
-        this.dispatcher = dispatcher;
     }
 }
