ca13b00e011c256ca96cee505b4c85b7b29409f9
6-LL <lorenzanilautaro+github@gmail.com>
2026-06-11 23:37:37 -0300

    SoundS2CPayload added
    (cherry picked from commit b2a649ed9c900f84d9d1a2a0ba3007f664fd5819)

---
+++ src/main/java/io/github/lau6l/chatdiag/network/SoundS2CPayload.java
@@ -0,0 +1,23 @@
+package io.github.lau6l.chatdiag.network;
+
+import io.github.lau6l.chatdiag.ChatDiag;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+public record SoundS2CPayload(String soundId) implements CustomPayload {
+    public static final Identifier SOUND_PAYLOAD_ID = ChatDiag.of("sound");
+    public static final CustomPayload.Id<SoundS2CPayload> ID = new Id<>(SOUND_PAYLOAD_ID);
+
+    public static final PacketCodec<RegistryByteBuf, SoundS2CPayload> CODEC = PacketCodec.tuple(
+            PacketCodecs.STRING, SoundS2CPayload::soundId,
+            SoundS2CPayload::new
+    );
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
