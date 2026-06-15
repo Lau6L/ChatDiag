4167e1184f177a17851672d4b1de44ce8c46aac4
6-LL <lorenzanilautaro+github@gmail.com>
2026-06-15 00:38:04 -0300

    SoundS2CPayload now uses Sound object
    (cherry picked from commit a4000782d8618e74a2d4d13c7fff61f6b29b8cb3)

--- src/main/java/io/github/lau6l/chatdiag/network/SoundS2CPayload.java
+++ src/main/java/io/github/lau6l/chatdiag/network/SoundS2CPayload.java
@@ -1,19 +1,18 @@
 package io.github.lau6l.chatdiag.network;
 
 import io.github.lau6l.chatdiag.ChatDiag;
+import io.github.lau6l.chatdiag.dialog.Sound;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 
-public record SoundS2CPayload(String soundId, float pitch) implements CustomPayload {
+public record SoundS2CPayload(Sound sound) implements CustomPayload {
     public static final Identifier SOUND_PAYLOAD_ID = ChatDiag.of("sound");
     public static final CustomPayload.Id<SoundS2CPayload> ID = new Id<>(SOUND_PAYLOAD_ID);
 
     public static final PacketCodec<RegistryByteBuf, SoundS2CPayload> CODEC = PacketCodec.tuple(
-            PacketCodecs.STRING, SoundS2CPayload::soundId,
-            PacketCodecs.FLOAT, SoundS2CPayload::pitch,
+            Sound.PACKET_CODEC_NO_POS, SoundS2CPayload::sound,
             SoundS2CPayload::new
     );
 
